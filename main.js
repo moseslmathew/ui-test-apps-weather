517d10f6d2ae2115d0d557f75207a6a9c6ec7778
moseslmathew <moseslmathew@gmail.com>
2023-06-06 07:18:22 +0530

    Commit-P

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { getWeather } from "./weather";
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-  (res) => {
-    console.log("DATA RETURNED",res)
-  }
-);
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then((data) => {rederCurrent(data.current)})
+  .catch((e) => {
+    console.log("Error", e);
+  });
+
+const rederCurrent = (data) => {
+  console.log("Current", data);
+  document.querySelector("[data-current-temp]").textContent=data.currentTepratrature;
+};
